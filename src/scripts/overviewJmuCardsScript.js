fe7fc48ac56412c15591f614eb0331986803363a
stephenender02 <stephenender02@gmail.com>
2025-11-01 16:11:19 -0600

    Add FAST job info

--- src/scripts/overviewJmuCardsScript.js
+++
@@ -1,7 +0,0 @@
-var acc = document.getElementsByClassName("involvementCard");
-
-for (var i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-  });
-}
